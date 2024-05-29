e6e662ec5350ae1fadabd226c2cc527469e4d6f4
Nicholas <nilouback@hotmail.com>
2024-05-29 11:42:24 -0300

    Add Icons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,14 @@
+import MenuElement from "./MenuElement";
+import { IconAdjust, IconDo, IconHome } from "./icons";
+
 export default function Menu() {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuElement url="/" texto="Início" icone={IconHome} />
+                <MenuElement url="/" texto="Ajustes" icone={IconAdjust} />
+                <MenuElement url="/" texto="Tarefas" icone={IconDo} />
+            </ul>
         </aside>
     )
 }
