6120a927490a64acd3907a020ccbba6d3186febc
Nicholas <nilouback@hotmail.com>
2024-06-05 17:06:55 -0300

    Formatting

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,19 @@
 import MenuElement from "./MenuElement";
+import Logo from "./Logo"
 import { IconHome, IconAdjust, IconDo } from "./icons";
 
 export default function Menu() {
     return (
         <aside>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-pink-500 to-purple-800
+                h-20 w-20
+            `}>
+            <Logo />
+            </div>
             <ul>
-                <MenuElement url="/index" texto="Início" icone={IconHome} />
+                <MenuElement url="/inicio" texto="Início" icone={IconHome} />
                 <MenuElement url="/ajustes" texto="Ajustes" icone={IconAdjust} />
                 <MenuElement url="/tarefas" texto="Tarefas" icone={IconDo} />
             </ul>
--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -7,12 +7,12 @@ export default function Titulo(props: TituloProps) {
     return (
         <div>
             <h1 className={`
-            
+                font-black text-3xl text-gray-800
             `}>
                 {props.titulo}
             </h1>
             <h2 className={`
-            
+                font-light text-sm text-gray-700
             `}>
                 {props.subtitulo}
             </h2>
