bb31ee673350893f3476f41b30ced10b8066444e
Nicholas <nilouback@hotmail.com>
2024-06-05 17:07:20 -0300

    Adding Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+    return(
+        <div className={`
+            flex flex-col h-12 w-12 rounded-full
+            bg-white
+        `}>
+
+        </div>
+    )
+}
