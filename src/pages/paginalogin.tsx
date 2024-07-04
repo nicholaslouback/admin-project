326b3c5b74a23a44500f81bf78014516588435b9
Nicholas <nilouback@hotmail.com>
2024-07-04 16:26:23 -0300

    Add Login

--- src/pages/paginalogin.tsx
+++ src/pages/paginalogin.tsx
@@ -16,11 +16,11 @@ export default function PaginaLogin() {
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <div className="m-10 w-full mb:w-1/2">
+            <div className="m-10 w-full md:w-1/2 lg:w-1/3">
                 <h1 className={`
-                      text-xl font-bold
+                      text-2xl font-bold
                     `}>
-                    {modo === 'login' ? 'Insira a Sua Conta' : 'Faça o Cadastro na Plataforma'}
+                    {modo === 'login' ? 'Insira a Sua Conta' : 'Entrar'}
                 </h1>
                 <h2 className={`mb-3`}>Continuar para Plataforma</h2>
                 <LoginInput
@@ -43,10 +43,27 @@ export default function PaginaLogin() {
                      `}>
                     {modo === 'login' ? 'Entrar' : 'Cadastrar'}
                 </button>
+                
+                {modo === 'login' ? (
+                    <p className="mt-8">
+                        Ainda não faz parte?
+                        <a onClick={() => setModo('cadastro')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer                       
+                        `}> Criar uma Conta</a>
+                    </p>
+                ):(
+                    <p className="mt-8">
+                        Já faz parte da plataforma?                
+                        <a onClick={() => setModo('login')} className={`
+                            text-blue-500 hover:text-blue-700 font-semibold cursor-pointer                       
+                        `}> Entre com seu Login</a>
+                    </p>
+                )}
+
             </div>
-            <div className="hidden md:block w-1/2">
+            <div className="hidden md:block w-1/2 lg:w-2/3">
                 <img src="https://picsum.photos/200/300" alt="Imagem Tela de Login" 
-                className=""/>
+                className="h-screen w-full object-cover"/>
             </div>
         </div>
     )
